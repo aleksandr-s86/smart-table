83afe0446a10be44e17944ed11c307e5732182fe
Alexander Sinev <alexandros.sinev@yandex.ru>
2026-06-16 13:24:09 +0300

    sorting done

--- src/main.js
+++ src/main.js
@@ -49,6 +49,7 @@ async function render(action) {
 
     query = applySearching(query, state, action)
     query = applyFiltering(query, state, action);
+    query = applySorting(query, state, action);
     query = applyPagination(query, state, action);
     
 

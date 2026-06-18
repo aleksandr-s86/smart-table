93b2f88e085a01ed1498c1d85ddb0f2fa36700d3
Alexander Sinev <alexandros.sinev@yandex.ru>
2026-06-18 17:40:58 +0300

    pagination step two

--- src/main.js
+++ src/main.js
@@ -45,9 +45,11 @@ async function render(action) {
     //result = applySearching(result, state, action);
     //result = applyFiltering(result, state, action);
     //result = applySorting(result, state, action);
-    //result = applyPagination(result, state, action);
+    query = applyPagination(query, state, action);
 
     const {total, items} = await api.getRecords(query);
+
+    updatePagination(total, query);
 
     sampleTable.render(items)
 }
@@ -72,7 +74,7 @@ const applySorting = initSorting([
     searchBySeller: indexes.sellers
 });*/
 
-const applyPagination = initPagination(
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,
     (el, page, isCurrent) => {
         const input = el.querySelector('input');
